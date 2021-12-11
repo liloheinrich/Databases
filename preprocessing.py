bd04ba983a991fd6fdff7cd6635c0da9101e21b2
Zoë McGinnis <zoekauai@gmail.com>
2021-12-11 14:40:39 -0500

    Add q (column headers)

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,89 @@
+"""
+column_headers: list of headers for csv, q in algorithm
+format_data: formats cleveland.csv into list of lists each containing row of 
+csv (d in algorithm), outputs tuple: (q, d)
+"""
+
 import csv
+
+column_headers = [
+    'id', 
+    'ccf',
+    'age',
+    'sex',
+    'painloc',
+    'painexer',
+    'relrest',
+    'pncaden',
+    'cp',
+    'trestbps',
+    'htn',
+    'chol',
+    'smoke',
+    'cigs',
+    'years',
+    'fbs',
+    'dm',
+    'famhist',
+    'restecg',
+    'ekgmo',
+    'ekgday',
+    'ekgyr',
+    'dig',
+    'prop',
+    'nitr',
+    'pro',
+    'diuretic',
+    'proto',
+    'thaldur',
+    'thaltime',
+    'met',
+    'thalach',
+    'thalrest',
+    'tpeakbps',
+    'tpeakbpd',
+    'dummy',
+    'trestbpd',
+    'exang',
+    'xhypo',
+    'oldpeak',
+    'slope',
+    'rldv5',
+    'rldv5e',
+    'ca',
+    'restckm',
+    'exerckm',
+    'restef',
+    'restwm',
+    'exeref',
+    'exerwm',
+    'thal',
+    'thalsev',
+    'thalpul',
+    'earlobe',
+    'cmo',
+    'cday',
+    'cyr',
+    'num',
+    'lmt',
+    'ladprox',
+    'laddist',
+    'diag',
+    'cxmain',
+    'ramus',
+    'om1',
+    'om2',
+    'rcaprox',
+    'rcadist',
+    'lvx1',
+    'lvx2',
+    'lvx3',
+    'lvx4',
+    'lvf',
+    'cathef',
+    'junk',
+    'name'
+]
 
 def format_data(data):
     """
@@ -33,4 +118,7 @@ def format_data(data):
                     sublist = []
 
         print(final_list)
-        return final_list
+        return (column_headers, final_list)
+
+if __name__ == "__main__":
+    data = format_data("cleveland.csv")
